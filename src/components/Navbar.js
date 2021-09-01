49280b5d4c9a4de1a62ca16f4d6deb0fd94baf48
HaiHart <89720219+HaiHart@users.noreply.github.com>
2021-09-01 09:10:31 +0700

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../logo.svg"
+import { ButtonContainer,NavWrapper } from './Styling'
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+				<Link to='/'>
+					<img src={logo} alt="home" className="navbar-brand" />
+				</Link>
+				<ul className="navbar-nav align-items-center" >
+					<li className="nav-item ms-5">
+						<Link to="/" className="nav-link">
+						store
+						</Link>
+					</li>
+				</ul>
+						<Link to='/Cart' className="ms-auto">
+					<ButtonContainer>
+						<span className="me-2">
+							<i className="fas fa-cart-plus" />
+						</span>
+						my cart
+					</ButtonContainer>
+				</Link>
+			</NavWrapper>
+		)
+	}
+}
